568ff0c4f49db72de93181eb4d1428c7a39a3fab
laosan-xx <xxhy1989@gmail.com>
2026-02-07 22:18:30 +0800

    2

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -51,6 +51,8 @@ UPDATE_PACKAGE "aurora-config" "eamonxg/luci-app-aurora-config" "master"
 UPDATE_PACKAGE "kucat" "sirpdboy/luci-theme-kucat" "master"
 UPDATE_PACKAGE "kucat-config" "sirpdboy/luci-app-kucat-config" "master"
 
+UPDATE_PACKAGE "v2ray-geodata" "laosan-xx/my-v2ray-geodata" "master"
+UPDATE_PACKAGE "frp" "laosan-xx/my-frp" "main"
 UPDATE_PACKAGE "homeproxy" "VIKINGYFY/homeproxy" "main"
 UPDATE_PACKAGE "momo" "nikkinikki-org/OpenWrt-momo" "main"
 UPDATE_PACKAGE "nikki" "nikkinikki-org/OpenWrt-nikki" "main"
@@ -117,5 +119,5 @@ UPDATE_VERSION() {
 }
 
 #UPDATE_VERSION "软件包名" "测试版，true，可选，默认为否"
-#UPDATE_VERSION "sing-box"
+UPDATE_VERSION "sing-box"
 #UPDATE_VERSION "tailscale"
